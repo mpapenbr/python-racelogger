c2b2a2f5287c6ccd4245b1474c6ae108b805b608
mpapenbr <Markus.Papenbrock@gmail.com>
2021-10-03 19:30:53 +0200

    chg: pkg: adjustments for release

--- newRelease.sh
+++ newRelease.sh
@@ -9,10 +9,13 @@ nextVersion=$(bump2version --dry-run --list $VERSION_PART --allow-dirty | grep n
 echo $nextVersion
 
 # windows cp1252 has some trouble piping the output if we don't do this
-PYTHONIOENCODING="UTF-8" BUMP_VERSION="$nextVersion ($(date +%Y-%m-%d))" gitchangelog > CHANGELOG.rst
+PYTHONIOENCODING="UTF-8" BUMP_VERSION="v$nextVersion ($(date +%Y-%m-%d))" gitchangelog > CHANGELOG.rst
 git add CHANGELOG.rst
 
-bump2version  --allow-dirty $VERSION_PART
+bump2version --allow-dirty  $VERSION_PART
+
+git push
+git push origin v$nextVersion
 
 # git commit -m "Bump $currentVersion -> $nextVersion"
 # git tag v$nextVersion -m "Bump $currentVersion -> $nextVersion"
