f62ee7beb46df601e1ecc986f3ad361164a2b2cd
mpapenbr <Markus.Papenbrock+github@gmail.com>
2023-01-14 10:02:00 +0100

    chore: bump dependencies, updated link in doc

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
     python_requires='>=3.10',
     install_requires=[
         'click>=8',
-        'autobahn==22.7.1',
+        'autobahn==22.12.1',
         'certifi',
         "pyirsdk"
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
